170e087598bdb3bef353ac3f2f39acf90cc02890
Andy Fox <andy@rushc.com>
2017-11-22 15:33:06 -0800

    directives in source code

--- matrixmultiply.cpp
+++ matrixmultiply.cpp
@@ -10,16 +10,21 @@
 #include "matrixmultiply.h"
 void 
 GetARowCoefficients(short A[N][M], short row_ix, short RC[M]){
+#pragma HLS INLINE
   //go through rows fish out coefficients
   GetARowCoefficients_label0:for (unsigned i=0 ; i < M; i++)
-    RC[i] = A[row_ix][i];
+
+#pragma HLS UNROLL
+RC[i] = A[row_ix][i];
 }
 
 
 void
 GetBRowCoefficients(short B[M][P], unsigned row, short BC[P]){
+#pragma HLS INLINE
   //go through B row fish out coefficieints
   GetBRowCoefficients_label0:for (unsigned i=0; i < P; i++){
+#pragma HLS UNROLL
     BC[i] = B[row][i];
   }
 }
@@ -29,6 +34,10 @@ GetBRowCoefficients(short B[M][P], unsigned row, short BC[P]){
 //Matrix Multiply in row order.
 
 void matrixmultiply(short A[N][M], short B[M][P], short C[N][P]){
+#pragma HLS ARRAY_PARTITION variable=C complete dim=2
+#pragma HLS ARRAY_PARTITION variable=B complete dim=2
+#pragma HLS ARRAY_PARTITION variable=A complete dim=2
+#pragma HLS PIPELINE
 
  row_loop: for (unsigned i=0; i < N; i++){ //A row matrix loop
 	 short a_row[M];
