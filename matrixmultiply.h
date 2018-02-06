701bbf69860476aee0001f6173a9c171f37ea921
Andy Fox <andy@rushc.com>
2018-02-06 04:49:45 -0800

    Increased size of matrix multiply array

--- matrixmultiply.h
+++ matrixmultiply.h
@@ -1,6 +1,6 @@
-#define M_SZ 14
-#define N_SZ 16
-#define P_SZ 15
+#define N_SZ 20
+#define M_SZ 15
+#define P_SZ 10
 #define SIZE_NM_IN_BYTES  (N_SZ*M_SZ*2)
 #define SIZE_MP_IN_BYTES  (M_SZ*P_SZ*2)
 #define SIZE_NP_IN_BYTES  (N_SZ*P_SZ*2)
